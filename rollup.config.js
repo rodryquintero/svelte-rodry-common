a5f4c2077cb27d7d0949c687ee69d079d2cc90a5
Ivan Quintero <rodryquintero@gmail.com>
2021-06-11 12:14:24 -0500

    trying to fix global names issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,18 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
 import css from "rollup-plugin-css-only";
-import pkg from './package.json';
+import pkg from "./package.json";
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
-	],
-	plugins: [
-		svelte(),
-		resolve(),
-		css()
-	]
+  input: "src/index.js",
+  output: [
+    { file: pkg.module, format: "es" },
+    { file: pkg.main, format: "umd", name }
+  ],
+  plugins: [svelte(), resolve(), css()],
 };
